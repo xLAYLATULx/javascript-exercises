5006bfef0245a0bf46e10d7514c01abce9511499
Laylatul Firdaus <laylatul.firdaus@outlook.com>
2025-02-24 03:08:31 +0000

    Completed Exercise 12 and Passed All Tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const getAge = function(death, birth){
+  if(!death){
+    death =  new Date().getFullYear();
+  }
+  return death-birth;
+}
 
-};
+const findTheOldest = function(person) {
+  return person.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+    const currentAge = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
