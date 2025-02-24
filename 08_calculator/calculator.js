a98f5efc8d7851f798e44265657f83dfd5d9a63c
Laylatul Firdaus <laylatul.firdaus@outlook.com>
2025-02-24 02:11:41 +0000

    Completed Exercise 8 and Passed All Tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+  if(array.length === 0) return 0;
+  for(let i=0; i<array.length; i++){
+    total += array[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  if(array.length === 0) return 0;
+  for(let i=0; i<array.length; i++){
+    total *= array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0) return 1;
+	let total = 1;
+  for(let i = 1; i<=num; i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
