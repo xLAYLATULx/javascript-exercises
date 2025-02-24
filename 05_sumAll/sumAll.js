8ac2b10369f23bcf8fa87f5c354ab328ccef307f
Laylatul Firdaus <laylatul.firdaus@outlook.com>
2025-02-24 01:24:52 +0000

    Completed Exercise 5 and Passed All Tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if(!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+  if(num1 < 0 || num2 < 0) return "ERROR";
+  let total = 0;
+  if(num1 <= num2){
+    for(let i=num1; i<=num2; i++){
+      total +=i;
+    }
+  } else{
+    for(let i=num2; i<=num1; i++){
+      total +=i;
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
