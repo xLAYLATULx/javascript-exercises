c944c9c440c7f344a7d345f16aa9ad505a7278fd
Laylatul Firdaus <laylatul.firdaus@outlook.com>
2025-02-24 02:34:53 +0000

    Completed Exercise 10 and Passed All Tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  let result = 1;
+  let first = 1;
+  let second = 1;
+  if(num < 0) return "OOPS";
+  if(num == 0) return 0;
+  if(num == 1 || num == 2) return 1;
+  for(let i = 2; i<num; i++){
+    result = first + second;
+    first = second;
+    second = result;
+  }
+  return result;
 };
 
 // Do not edit below this line
