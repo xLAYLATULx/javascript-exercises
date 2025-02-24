d01fdc51172e4e34bd731e480915ed41191dd8f8
Laylatul Firdaus <laylatul.firdaus@outlook.com>
2025-02-24 01:10:40 +0000

    Completed Exercise 4 and Passed All Tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  const newArray = [];
+  array.forEach((element) => {
+    if (!args.includes(element)){
+      newArray.push(element);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
