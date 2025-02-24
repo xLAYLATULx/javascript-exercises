9f8f2929c1388477e990aa6f0ba6aa27988cd7a5
Laylatul Firdaus <laylatul.firdaus@outlook.com>
2025-02-24 00:55:53 +0000

    Completed Exercise 3 and Passed All Tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+  let reverse = "";
+  for(let i=0; i<=string.length; i++){
+    reverse += string.charAt(string.length-i);
+  }
+  return reverse;
 };
 
 // Do not edit below this line
